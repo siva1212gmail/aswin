b2a7e331502317aebaa1d245e789db49692041e7
siva1212gmail <138274603+siva1212gmail@users.noreply.github.com>
2025-10-01 16:34:33 +0530

    updated response

--- pintoshop/auth_user/views.py
+++ pintoshop/auth_user/views.py
@@ -38,7 +38,7 @@ class SendOTPView(APIView):
           
                 pass
 
-            return Response({"status":True,"message":"otp send succesfully","response" : {}})
+            return Response({"status":True,"message":"otp send succesfully","response" : {"otp":otp_code}})
         except Exception as e:
 
             return Response(
@@ -46,50 +46,50 @@ class SendOTPView(APIView):
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
 
-#
-#
-# class VerifyOTPView(APIView):
-#     def post(self, request):
-#         serializer = VerifyOTPSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         otp_id = serializer.validated_data['lng_OTP_ID']
-#         input_otp = serializer.validated_data['otp_code']
-#
-#         try:
-#             otp_obj = OTPVerification.objects.get(lng_OTP_ID=otp_id)
-#         except OTPVerification.DoesNotExist:
-#             return Response({"verified": False, "error": "Invalid OTP ID"}, status=404)
-#
-#         if otp_obj.str_OTP_Code != input_otp:
-#             return Response({"verified": False, "error": "Invalid OTP"}, status=400)
-#
-#         if timezone.now() - otp_obj.dte_Sent_Time > timedelta(minutes=5):
-#             return Response({"verified": False, "error": "OTP expired"}, status=400)
-#
-#         return Response({"verified": True})
 
 
 class VerifyOTPView(APIView):
     def post(self, request):
-        to_number = request.data.get('contact_value')
-        otp_code = request.data.get('otp_code')
-        account_sid = 'AC7eeefd0122473cc995d24ff86a5fe6a5'
-        auth_token = "cf730b95663087cb20a2e7fa6d77b6f3"
-        verify_sid = "VAcf0310873fc863a31683281836c49baf" 
-
-        client = Client(account_sid, auth_token)
+        serializer = VerifyOTPSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        otp_id = serializer.validated_data['lng_OTP_ID']
+        input_otp = serializer.validated_data['otp_code']
 
         try:
-            verification_check = client.verify.services(verify_sid).verification_checks.create(
-                to=to_number,
-                code=otp_code
-            )
-            print(verification_check)
-            print("Verification status:", verification_check.status)
+            otp_obj = OTPVerification.objects.get(lng_OTP_ID=otp_id)
+        except OTPVerification.DoesNotExist:
+            return Response({"verified": False, "error": "Invalid OTP ID"}, status=404)
 
-            if verification_check.status == 'approved':
-                return Response({"status":True,"message":"otp verified succesfully","response" : {}})
-            else:
-                return Response({"status":False,"message":"invaid otp","response" : {}})
-        except Exception as e:
-            return Response({"status":False,"message":"something went wrong","response" : {e}})
+        if otp_obj.str_OTP_Code != input_otp:
+            return Response({"verified": False, "error": "Invalid OTP"}, status=400)
+
+        if timezone.now() - otp_obj.dte_Sent_Time > timedelta(minutes=5):
+            return Response({"verified": False, "error": "OTP expired"}, status=400)
+
+        return Response({"verified": True})
+
+
+# class VerifyOTPView(APIView):
+#     def post(self, request):
+#         to_number = request.data.get('contact_value')
+#         otp_code = request.data.get('otp_code')
+#         account_sid = 'AC7eeefd0122473cc995d24ff86a5fe6a5'
+#         auth_token = "cf730b95663087cb20a2e7fa6d77b6f3"
+#         verify_sid = "VAcf0310873fc863a31683281836c49baf" 
+
+#         client = Client(account_sid, auth_token)
+
+#         try:
+#             verification_check = client.verify.services(verify_sid).verification_checks.create(
+#                 to=to_number,
+#                 code=otp_code
+#             )
+#             print(verification_check)
+#             print("Verification status:", verification_check.status)
+
+#             if verification_check.status == 'approved':
+#                 return Response({"status":True,"message":"otp verified succesfully","response" : {}})
+#             else:
+#                 return Response({"status":False,"message":"invaid otp","response" : {}})
+#         except Exception as e:
+#             return Response({"status":False,"message":"something went wrong","response" : {e}})
